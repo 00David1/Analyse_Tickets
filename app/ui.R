53cc83a804fe4e3527dcb45ce7a3cdf14ebd23ef
00David1 <davidgrondin103@gmail.com>
2024-03-23 11:49:00 +0100

    ajout du formulaire

--- app/ui.R
+++ app/ui.R
@@ -1,38 +1,39 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(shinythemes)
 
-# Define UI for application that draws a histogram
+data = read.csv("data.csv", sep = ";")
+
+choices_utilisateur <- c("Tout", unique(data$Utilisateur[!is.na(data$Utilisateur)]))
+choices_technicien <- c("Tout", unique(data$Technicien[!is.na(data$Technicien)]))
+choices_etat <- c("Tout", unique(data$Etats[!is.na(data$Etats)]))
+choices_urgence <- c("Tout", unique(data$Urgence[!is.na(data$Urgence)]))
+
 fluidPage(
   theme = shinytheme("flatly"),
   
   tabsetPanel(
-    tabPanel("Analyse des tickets")
-  ),
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+    tabPanel("Analyse des tickets",
+             sidebarLayout(
+               sidebarPanel(
+                 fluidRow(
+                   column(12, selectInput("utilisateur", "Utilisateur : ", choices = choices_utilisateur))
+                 ),
+                 fluidRow(
+                   column(12, selectInput("technicien", "Technicien : ", choices = choices_technicien))
+                 ),
+                 fluidRow(
+                   column(12, selectInput("etat", "Etat : ", choices = choices_etat))
+                 ),
+                 fluidRow(
+                   column(12, selectInput("urgence", "Urgence : ", choices = choices_urgence))
+                 )
+               ),
+               mainPanel(
+                 wellPanel(
+                   h4("Résultats :"),
+                 )
+               )
+             )
+  )
+ )
 )
